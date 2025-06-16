62074567a229d5d623dc5240afac3fa7efaec8d2
BotSolo621 <jl22062@my.westlake.school.nz>
2025-06-16 12:00:27 +1200

    Added default lists

---
+++ main.py
@@ -0,0 +1,59 @@
+#Imports
+import easygui
+
+#Data
+users = {
+    "JSM": {
+        "name": "John Smith",
+        "email": "John@techvision.com"
+    },
+    "JLO":{
+        "name": "Jane Love",
+        "email": "John@techvision.com"
+    },
+    "BDI":{
+        "name": "Bob Dillon",
+        "email": "Bob@techvision.com"
+    }
+}
+
+tasks = {
+    "T1": {
+        "title" : "Design Homepage",
+        "description": "Create a mockup of the homepage",
+        "priority": "3",
+        "progress": "In Progress"
+    },
+    "T2": {
+        "title" : "Implement Login page",
+        "description": "Create the Login page for the website",
+        "priority": "3",
+        "progress": "Blocked"
+    },
+    "T3": {
+        "title" : "Fix navigation menu",
+        "description": "Fix the navigation menu to be more user-friendly",
+        "priority": "1",
+        "progress": "Not Started"
+    },
+    "T4": {
+        "title" : "Add payment processing",
+        "description": "Implement payment processing for the website",
+        "priority": "2",
+        "progress": "In Progress"
+    },
+    "T5": {
+        "title" : "Create an About Us page",
+        "description": "Create a page with information about the company",
+        "priority": "1",
+        "progress": "Blocked"
+    },
+}
+
+def startUp():
+    if  currentUser == None:
+        pass
+
+
+currentUser = None
+startUp()
