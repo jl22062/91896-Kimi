8ac77c65d245884447a9a8318f9cc16c06485dfa
Kimi <jl22062@my.westlake.school.nz>
2025-06-23 14:08:15 +1200

    Fixed add task function aswell as list task function

--- main.py
+++ main.py
@@ -32,25 +32,29 @@ tasks = {
         "title" : "Implement Login page",
         "description": "Create the Login page for the website",
         "priority": 3,
-        "status": "Blocked"
+        "status": "Blocked",
+        "assignee": "JLO"
     },
     "T3": {
         "title" : "Fix navigation menu",
         "description": "Fix the navigation menu to be more user-friendly",
         "priority": 1,
-        "status": "Not Started"
+        "status": "Not Started",
+        "assignee": "BDI"
     },
     "T4": {
         "title" : "Add payment processing",
         "description": "Implement payment processing for the website",
         "priority": 2,
-        "status": "In Progress"
+        "status": "In Progress",
+        "assignee": "BDI"
     },
     "T5": {
         "title" : "Create an About Us page",
         "description": "Create a page with information about the company",
         "priority": 1,
-        "status": "Blocked"
+        "status": "Blocked",
+        "assignee": "JSM"
     },
 }
 
@@ -63,38 +67,59 @@ def updateTask():
 
 def newTask():
     while True:
-        fields = ["Title", "Description","Priority","Status"]
-        info = gui.multenterbox("Please enter task details","Add new tasks", fields)
+        fields = ["Title", "Description","Priority","Status", "Assignee"]
+        info = gui.multenterbox(
+            "Please enter task details","Add new tasks",
+            fields
+            )
         if info is None:
             return
-        title, desc, priority, status = info[0].strip(),info[1],info[2],info[3]
-        if not title or not desc or not priority or not status:
+        title, desc, priority, status, assignee = info[0].strip(),info[1],info[2],info[3],info[4]
+        if not title or not desc or not priority or not status or not assignee:
             gui.msgbox("Please enter all values", "Error")
             continue
         elif not priority.isdigit():
-            gui.msgbox("Priority must be a number", "Error")
+            gui.msgbox("Priority must be a positive whole number", "Error")
             continue
         elif title in tasks:
             gui.msgbox("Title already exists", "Error")
             continue
         taskID = "T" + str(len(tasks) + 1)
-        tasks[taskID] = {"title":title, "description":desc, "priority":priority, "status":status}
+        tasks[taskID] = {
+            "title":title, 
+            "description":desc, 
+            "priority":priority, 
+            "status":status,
+            "assignee":assignee
+            }
         return
 
 def listTasks():
     title  ="Task manager"
     msg = "Select your task:"
 
-    for i in tasks:
-        taskNames = [tasks[i]["title"] for i in tasks]
-    choice = gui.multchoicebox(msg, title, taskNames)
+    taskNames = [tasks[task]["title"] for task in tasks]
+    choice = gui.choicebox(msg, title, taskNames)
+    for taskID in tasks:
+        if tasks[taskID]["title"] == choice:
+            gui.msgbox(title=tasks[taskID]['title'], 
+                       msg=f"Title: {tasks[taskID]['title']}\nDescription:" \
+                       f"{tasks[taskID]['description']}\nPriority:"\
+                        f"{tasks[taskID]['priority']}\nStatus:"\
+                            f"{tasks[taskID]['status']}\nAssignee:"\
+                                f"{tasks[taskID]['assignee']}")
+
     if choice is None:
         return
 
 def startUp():
     while True:
-        choices = ["List task","New task","a"]
-        init = gui.buttonbox("Welcome to [Generic Task Manager], what would you like to do today?","Task manager",choices)
+        choices = ["List task","New task","aP"]
+        init = gui.buttonbox(
+            "Welcome to [Generic Task Manager], " \
+            "what would you like to do today?","Task manager",
+            choices
+            )
         if init == "List task":
             listTasks()
         elif init == "New task":
