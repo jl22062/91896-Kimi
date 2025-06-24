75304f5415a74be52206ed83c8f513ba756e45bc
Kimi <jl22062@my.westlake.school.nz>
2025-06-24 12:49:18 +1200

    Updated search task function, working on edit task function.

--- main.py
+++ main.py
@@ -60,28 +60,70 @@ tasks = {
 
 def searchTask():
     searchTerms = gui.enterbox("Please enter what you want to search")
+    found = False
     for taskID in tasks:
-        print(taskID)
-        if searchTerms.lower() == taskID.lower():
-            gui.msgbox(msg=f"Found task with the ID: {searchTerms}\n\n"\
-                       f"Title: {tasks[taskID]['title']}\nDescription:" \
-                       f"{tasks[taskID]['description']}\nPriority:"\
-                        f"{tasks[taskID]['priority']}\nStatus:"\
-                            f"{tasks[taskID]['status']}\nAssignee:"\
-                                f"{tasks[taskID]['assignee']}")
-        elif searchTerms.lower() == tasks[taskID]["title".lower()]:
-            print(searchTerms)
-            gui.msgbox(msg=f"Found task with the title: {searchTerms}\n\n"\
-                       f"Title: {tasks[taskID]['title']}\nDescription:" \
-                       f"{tasks[taskID]['description']}\nPriority:"\
-                        f"{tasks[taskID]['priority']}\nStatus:"\
-                            f"{tasks[taskID]['status']}\nAssignee:"\
-                                f"{tasks[taskID]['assignee']}")
+        if searchTerms is None:
+            return
 
-        else:
-            gui.msgbox("Task not found")
+        elif searchTerms.lower() == taskID.lower():
+            gui.msgbox(msg=f'Found task with the ID: "{searchTerms}"\n\n'\
+                           f"Title: {tasks[taskID]['title']}\nDescription:" \
+                           f"{tasks[taskID]['description']}\nPriority:"\
+                           f"{tasks[taskID]['priority']}\nStatus:"\
+                           f"{tasks[taskID]['status']}\nAssignee:"\
+                           f"{tasks[taskID]['assignee']}")
+            found = True
+            break
+
+        elif searchTerms.lower() == tasks[taskID]["title"].lower():
+            gui.msgbox(msg=f'Found task with the title: "{searchTerms}"\n\n'\
+                           f"Title: {tasks[taskID]['title']}\nDescription:" \
+                           f"{tasks[taskID]['description']}\nPriority:"\
+                           f"{tasks[taskID]['priority']}\nStatus:"\
+                           f"{tasks[taskID]['status']}\nAssignee:"\
+                           f"{tasks[taskID]['assignee']}")
+            found = True
+            break
+    if found == False:
+        gui.msgbox("Task not found.")
+        
 def updateTask():
-    pass
+    title  ="Task manager"
+    msg = "Select the task you want to update:"
+
+    taskNames = [tasks[task]["title"] for task in tasks]
+    edit = gui.choicebox(msg, title, taskNames)
+    for taskID in tasks:
+        if tasks[taskID]["title"] == edit:
+            fields = ["Title", "Description","Priority","Status", "Assignee"]
+            info = gui.multenterbox(
+                "Please enter task's new details","update task",fields)
+            if info is None:
+                return
+            title, desc, priority = info[0],info[1],info[2]
+            status, assignee = info[3],info[4]
+            if not title or not desc or not priority or not status or not assignee:
+                gui.msgbox("Please enter all values", "Error")
+                continue
+            elif not priority.isdigit():
+                gui.msgbox("Priority must be a positive whole number", "Error")
+                continue
+            elif title != tasks:
+                gui.msgbox("Title doesn't exist yet, try creating a new task"\
+                    " instead", "Error")
+                continue
+            taskID = f"T{str(len(tasks) + 1)}"
+            tasks[taskID] = {
+                "title":title, 
+                "description":desc, 
+                "priority":priority, 
+                "status":status,
+                "assignee":assignee
+                }
+            return
+
+    if edit is None:
+        return
 
 def newTask():
     while True:
@@ -92,7 +134,8 @@ def newTask():
             )
         if info is None:
             return
-        title, desc, priority, status, assignee = info[0].strip(),info[1],info[2],info[3],info[4]
+        title, desc, priority = info[0],info[1],info[2]
+        status, assignee = info[3],info[4]
         if not title or not desc or not priority or not status or not assignee:
             gui.msgbox("Please enter all values", "Error")
             continue
@@ -102,7 +145,7 @@ def newTask():
         elif title in tasks:
             gui.msgbox("Title already exists", "Error")
             continue
-        taskID = "T" + str(len(tasks) + 1)
+        taskID = f"T{str(len(tasks) + 1)}"
         tasks[taskID] = {
             "title":title, 
             "description":desc, 
@@ -122,17 +165,17 @@ def listTasks():
         if tasks[taskID]["title"] == choice:
             gui.msgbox(title=tasks[taskID]['title'], 
                        msg=f"Title: {tasks[taskID]['title']}\nDescription:" \
-                       f"{tasks[taskID]['description']}\nPriority:"\
-                        f"{tasks[taskID]['priority']}\nStatus:"\
-                            f"{tasks[taskID]['status']}\nAssignee:"\
-                                f"{tasks[taskID]['assignee']}")
+                           f"{tasks[taskID]['description']}\nPriority:"\
+                           f"{tasks[taskID]['priority']}\nStatus:"\
+                           f"{tasks[taskID]['status']}\nAssignee:"\
+                           f"{tasks[taskID]['assignee']}")
 
     if choice is None:
         return
 
 def startUp():
     while True:
-        choices = ["List task","New task","Search task"]
+        choices = ["List task","New task","Search task", "Update task", "Exit"]
         init = gui.buttonbox(
             "Welcome to [Generic Task Manager], " \
             "what would you like to do today?","Task manager",
@@ -144,6 +187,10 @@ def startUp():
             newTask()
         elif init == "Search task":
             searchTask()
+        elif init == "Update task":
+            updateTask()
+        elif init == "Exit":
+            exit()
         elif init is None:
             exit()
         
